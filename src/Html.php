40ad2608636f469ed03bca266bdffd54ac4311d8
Alexandr Katrazhenko <katrazhenko@gmail.com>
2020-11-28 17:55:07 +0200

    up

---
+++ src/Html.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace app\components;
+
+class Html extends \yii\helpers\Html
+{
+    public static function __callStatic($name, $args)
+    {
+        if (stripos($name, 'begin') === 0){
+            echo self::beginTag(strtolower(substr($name, 4)), $args);
+        }
+        if (stripos($name, 'end') === 0){
+            echo self::endTag(strtolower(substr($name, 2)));
+        }
+        echo self::tag($name, $args[0], $args[1]);
+    }
+
+    public function __call($name, $args)
+    {
+        if (stripos($name, 'begin') === 0){
+            echo self::beginTag(strtolower(substr($name, 4)), $args);
+        }
+        if (stripos($name, 'end') === 0){
+            echo self::endTag(strtolower(substr($name, 2)));
+        }
+        echo self::tag($name, $args[0], $args[1]);
+    }
+
+
+}
